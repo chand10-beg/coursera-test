9f929a12a97378bec50410329b07fd434af2efb3
chand10-beg <62929357+chand10-beg@users.noreply.github.com>
2020-07-09 15:06:37 +0530

    Add files via upload

---
+++ site/script.js
@@ -0,0 +1,35 @@
+(function (window) {
+
+Hello Yaakov
+Good Bye John
+Good Bye Jen
+Good Bye Jason
+Hello Paul
+Hello Frank
+Hello Larry
+Hello Paula
+Hello Laura
+Good Bye Jim
+WARNING!!! WARNING!!!
+The code does NOT currently work! It is YOUR job to make it work
+as described in the requirements and the steps in order to complete this
+assignment.
+WARNING!!! WARNING!!!
+*/
+
+
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+
+for (var i in names) {
+
+  var firstLetter = ((names[i]).charAt(0)).toLowerCase();
+
+  if (firstLetter == 'j') {
+     window.byeSpeaker.speak(names[i]);
+  } else {
+     window.helloSpeaker.speak(names[i]);
+  }
+}
+})(window);
